e0cd27c06580d60db77b838526c51ab154ff01f0
Metehan Kaya <kayahanmete96@gmail.com>
2018-03-17 18:44:08 +0300

    Added Knockout

---
+++ src/controller/KnockoutController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import model.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class KnockoutController {
+
+}

473a848eb5df70753471edf796912d26855ff9ea
Kerem Ayöz <keremayoz@hotmail.com>
2018-03-17 18:55:17 +0300

    Model class added

---
+++ src/model/Player.java
@@ -0,0 +1,220 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Kerem
+ *
+ */
+public class Player extends Person {
+
+	private String position;
+	private int overall;
+	private char foot;
+	private int salary;
+	private int value;
+	private boolean availableWeek;
+	private int[] attributes;
+	private ArrayList<Action> actions;
+	
+	private int cntMatch;
+	private int cntGoal;
+	private int cntAssist;
+	private int cntYellowCard;
+	private int cntRedCard;
+
+	/**
+	 * @param name
+	 * @param age
+	 * @param height
+	 * @param weight
+	 * @param nationality
+	 * @param position
+	 * @param overall
+	 * @param foot
+	 * @param salary
+	 * @param value
+	 * @param availableWeek
+	 * @param attributes
+	 * @param actions
+	 */
+	public Player(String name, int age, int height, int weight, String nationality, String position, int overall,
+			char foot, int salary, int value, boolean availableWeek, int[] attributes, ArrayList<Action> actions) {
+		super(name, age, height, weight, nationality);
+		this.position = position;
+		this.overall = overall;
+		this.foot = foot;
+		this.salary = salary;
+		this.value = value;
+		this.availableWeek = availableWeek;
+		this.attributes = attributes;
+		this.actions = actions;
+		
+		this.cntMatch = 0;
+		this.cntGoal = 0;
+		this.cntAssist = 0;
+		this.cntYellowCard = 0;
+		this.cntRedCard = 0;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public String getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position
+	 *            the position to set
+	 */
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the overall
+	 */
+	public int getOverall() {
+		return overall;
+	}
+
+	/**
+	 * @param overall
+	 *            the overall to set
+	 */
+	public void setOverall(int overall) {
+		this.overall = overall;
+	}
+
+	/**
+	 * @return the foot
+	 */
+	public char getFoot() {
+		return foot;
+	}
+
+	/**
+	 * @param foot
+	 *            the foot to set
+	 */
+	public void setFoot(char foot) {
+		this.foot = foot;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public int getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary
+	 *            the salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the availableWeek
+	 */
+	public boolean isAvailableWeek() {
+		return availableWeek;
+	}
+
+	/**
+	 * @param availableWeek
+	 *            the availableWeek to set
+	 */
+	public void setAvailableWeek(boolean availableWeek) {
+		this.availableWeek = availableWeek;
+	}
+
+	/**
+	 * @return the attributes
+	 */
+	public int[] getAttributes() {
+		return attributes;
+	}
+
+	/**
+	 * @param attributes
+	 *            the attributes to set
+	 */
+	public void setAttributes(int[] attributes) {
+		this.attributes = attributes;
+	}
+
+	/**
+	 * @return the actions
+	 */
+	public ArrayList<Action> getActions() {
+		return actions;
+	}
+
+	/**
+	 * @param actions
+	 *            the actions to set
+	 */
+	public void setActions(ArrayList<Action> actions) {
+		this.actions = actions;
+	}
+
+	public int getCntMatch() {
+		return cntMatch;
+	}
+
+	public void setCntMatch(int cntMatch) {
+		this.cntMatch = cntMatch;
+	}
+
+	public int getCntGoal() {
+		return cntGoal;
+	}
+
+	public void setCntGoal(int cntGoal) {
+		this.cntGoal = cntGoal;
+	}
+
+	public int getCntAssist() {
+		return cntAssist;
+	}
+
+	public void setCntAssist(int cntAssist) {
+		this.cntAssist = cntAssist;
+	}
+
+	public int getCntYellowCard() {
+		return cntYellowCard;
+	}
+
+	public void setCntYellowCard(int cntYellowCard) {
+		this.cntYellowCard = cntYellowCard;
+	}
+
+	public int getCntRedCard() {
+		return cntRedCard;
+	}
+
+	public void setCntRedCard(int cntRedCard) {
+		this.cntRedCard = cntRedCard;
+	}
+
+}
---
+++ src/model/Team.java
@@ -0,0 +1,253 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Kerem
+ *
+ */
+public class Team {
+
+	private String name;
+	private String color;
+	private String tactic;
+	private String stadium;
+	private String history;
+	private String nationality;
+	private String nick;
+	private double stars;
+	private int style;
+	private int tempo;
+	private Manager manager;
+	private President president;
+	private ArrayList<Player> players;
+
+	/**
+	 * @param name
+	 * @param color
+	 * @param tactic
+	 * @param stadium
+	 * @param history
+	 * @param nationality
+	 * @param nick
+	 * @param stars
+	 * @param style
+	 * @param tempo
+	 * @param manager
+	 * @param president
+	 * @param players
+	 */
+	public Team(String name, String color, String tactic, String stadium, String history, String nationality,
+			String nick, double stars, int style, int tempo, Manager manager, President president,
+			ArrayList<Player> players) {
+		this.name = name;
+		this.color = color;
+		this.tactic = tactic;
+		this.stadium = stadium;
+		this.history = history;
+		this.nationality = nationality;
+		this.nick = nick;
+		this.stars = stars;
+		this.style = style;
+		this.tempo = tempo;
+		this.manager = manager;
+		this.president = president;
+		this.players = players;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the color
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * @param color
+	 *            the color to set
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return the tactic
+	 */
+	public String getTactic() {
+		return tactic;
+	}
+
+	/**
+	 * @param tactic
+	 *            the tactic to set
+	 */
+	public void setTactic(String tactic) {
+		this.tactic = tactic;
+	}
+
+	/**
+	 * @return the stadium
+	 */
+	public String getStadium() {
+		return stadium;
+	}
+
+	/**
+	 * @param stadium
+	 *            the stadium to set
+	 */
+	public void setStadium(String stadium) {
+		this.stadium = stadium;
+	}
+
+	/**
+	 * @return the history
+	 */
+	public String getHistory() {
+		return history;
+	}
+
+	/**
+	 * @param history
+	 *            the history to set
+	 */
+	public void setHistory(String history) {
+		this.history = history;
+	}
+
+	/**
+	 * @return the nationality
+	 */
+	public String getNationality() {
+		return nationality;
+	}
+
+	/**
+	 * @param nationality
+	 *            the nationality to set
+	 */
+	public void setNationality(String nationality) {
+		this.nationality = nationality;
+	}
+
+	/**
+	 * @return the nick
+	 */
+	public String getNick() {
+		return nick;
+	}
+
+	/**
+	 * @param nick
+	 *            the nick to set
+	 */
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+	/**
+	 * @return the stars
+	 */
+	public double getStars() {
+		return stars;
+	}
+
+	/**
+	 * @param stars
+	 *            the stars to set
+	 */
+	public void setStars(double stars) {
+		this.stars = stars;
+	}
+
+	/**
+	 * @return the style
+	 */
+	public int getStyle() {
+		return style;
+	}
+
+	/**
+	 * @param style
+	 *            the style to set
+	 */
+	public void setStyle(int style) {
+		this.style = style;
+	}
+
+	/**
+	 * @return the tempo
+	 */
+	public int getTempo() {
+		return tempo;
+	}
+
+	/**
+	 * @param tempo
+	 *            the tempo to set
+	 */
+	public void setTempo(int tempo) {
+		this.tempo = tempo;
+	}
+
+	/**
+	 * @return the manager
+	 */
+	public Manager getManager() {
+		return manager;
+	}
+
+	/**
+	 * @param manager
+	 *            the manager to set
+	 */
+	public void setManager(Manager manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * @return the president
+	 */
+	public President getPresident() {
+		return president;
+	}
+
+	/**
+	 * @param president
+	 *            the president to set
+	 */
+	public void setPresident(President president) {
+		this.president = president;
+	}
+
+	/**
+	 * @return the players
+	 */
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	/**
+	 * @param players
+	 *            the players to set
+	 */
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
+
+}

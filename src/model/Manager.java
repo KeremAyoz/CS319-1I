014e14d9f292ddd7e32c088d16209ba2fe48b53f
Babanazar Gutlygeldiyev <mathik_96@mail.ru>
2018-03-17 19:01:25 +0300

    model class are added

---
+++ src/model/Manager.java
@@ -0,0 +1,53 @@
+package model;
+
+/**
+ * @author Kerem
+ *
+ */
+public class Manager extends Person {
+
+	private int overall;
+	private int experience;
+
+	/**
+	 * @param name
+	 * @param age
+	 * @param height
+	 * @param weight
+	 * @param nationality
+	 */
+	public Manager(String name, int age, int height, int weight, String nationality, int overall, int experience) {
+		super(name, age, height, weight, nationality);
+		this.overall = overall;
+		this.experience = experience;
+	}
+
+	/**
+	 * @return the overall
+	 */
+	public int getOverall() {
+		return overall;
+	}
+
+	/**
+	 * @param overall the overall to set
+	 */
+	public void setOverall(int overall) {
+		this.overall = overall;
+	}
+
+	/**
+	 * @return the experience
+	 */
+	public int getExperience() {
+		return experience;
+	}
+
+	/**
+	 * @param experience the experience to set
+	 */
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	
+}
---
+++ src/model/President.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * @author Kerem
+ *
+ */
+public class President extends Person {
+
+	/**
+	 * @param name
+	 * @param age
+	 * @param height
+	 * @param weight
+	 * @param nationality
+	 */
+	public President(String name, int age, int height, int weight, String nationality) {
+		super(name, age, height, weight, nationality);
+		// TODO Auto-generated constructor stub
+	}
+
+}

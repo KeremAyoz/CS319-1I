d688521e4046e0004431a06ef6e7d17292474a60
Metehan Kaya <kayahanmete96@gmail.com>
2018-03-17 18:43:45 +0300

    Added TopMenu

---
+++ src/controller/TopMenu.java
@@ -0,0 +1,71 @@
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import model.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+public class TopMenu implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+
+	@FXML
+	public void teamClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/TeamView.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void tacticClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/TacticView.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void groupClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/GroupView.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void knockoutClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/Knockout.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void statClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/StatisticView.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void calendarClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/CalendarView.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void saveClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/HomeScreen.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+
+	@FXML
+	public void continueClicked() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/MatchStart.fxml"));
+		Main.getMainStage().setScene(new Scene(root));
+	}
+}
